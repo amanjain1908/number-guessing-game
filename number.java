197295f2abef574546fd7660fdf109d1b73f8289
amanjain1908 <121744936+amanjain1908@users.noreply.github.com>
2023-10-10 22:34:43 +0530

    Update number.java

--- number.java
+++ number.java
@@ -11,31 +11,21 @@ class abc {
 		int i;
 
 		System.out.println(
-			"A number is chosen"
-			+ " between 1 to 100."
-			+ "Guess the number"
-			+ " within 5 trials.");
+			"A number is chosen between 1 to 100. Guess the number within 5 trials.");
 
 		for (i = 0; i < K; i++) {
-			System.out.println(
-				"Guess the number:");
+			System.out.println("Guess the number:");
 			int guess = sc.nextInt();
 
 			if (number == guess) {
-				System.out.println(
-					"Congratulations!"
-					+ " You guessed the number.");
+				System.out.println("Congratulations! You guessed the number.");
 				break;
 			}
 			else if (number > guess&& i != K - 1) {
-				System.out.println(
-					"The number is "
-					+ "greater than " + guess);
+				System.out.println("The number is greater than " + guess);
 			}
 			else if (number < guess && i != K - 1) {
-				System.out.println(
-					"The number is"
-					+ " less than " + guess);
+				System.out.println("The number is less than " + guess);
 			}
 		}
 
@@ -45,8 +35,6 @@ class abc {
 			System.out.println("The number was " + number);
 		}
 	}
-
-
 	public static void main(String arg[])
 	{
 		guessingNumberGame();
